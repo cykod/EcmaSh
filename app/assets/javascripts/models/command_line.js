8e310c4b2dc12ab86cb79df83684154457ff94da
Pascal Rettig <pascal@cykod.com>
2013-11-15 08:37:30 -0500

    Refactor WIP

--- app/assets/javascripts/models/command_line.js
+++ app/assets/javascripts/models/command_line.js
@@ -6,9 +6,13 @@
       this.command = options.command;
 
       var self = this;
-      this.command.on("error",function() { self.trigger("error",self); });
-      this.command.on("ran",function() { self.trigger("ran",self); });
-
+      this.command.on("error",function() { 
+        self.trigger("error",self); 
+      });
+      this.command.on("ran",function() { 
+        self.trigger("ran",self);
+        self.collection.add(new EcmaSh.Result(self.command.get("result")));
+      });
     }
 
   });
