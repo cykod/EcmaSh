e941f70c206c84e3894417500eb10d6ff54f9ffa
Pascal Rettig <pascal@cykod.com>
2013-12-08 08:01:30 -0500

    Cp Fix

--- app/models/command/cp.rb
+++ app/models/command/cp.rb
@@ -1,35 +1,32 @@
 class Command::Cp < ::Command
   def run
+
+    # get the source node
     source_node_path = resolve_path(argv[0])
     source_node = Node.fetch(source_node_path)
 
+    # get get the destination node and directory
     destination_node_path = resolve_path(argv[1])
     destination_node = Node.fetch(destination_node_path)
 
-    result_node = nil
-
-    raise InvalidFileError.new(source_node_path) unless access.write?(source_node)
-
-    # check if destination node exists, and is a directory we can write to
-    if(destination_node && access.write?(destination_node))
-      if destination_node.directory?
-        source_node.parent = destination_node
-        source_node.save
-        result_node = source_node
-      end
-    else 
-      directory, file_name = resolve_directory_and_file(argv[1])
-
-      destination_directory = Node.fetch(directory)
-
-      if(access.write?(destination_directory)) 
-        result_node = source_node.copy(destination_directory,file_name)
-      else
-        raise CantWriteError.new(destination_node_path)
-      end
+    if !destination_node
+      destination_directory_name, destination_file_name = resolve_directory_and_file(argv[1])
+      destination_directory = Node.fetch(destination_directory_name)
+    elsif destination_node.directory?
+      destination_directory = destination_node
+      destination_file_name = source_node.name
+      destination_node = nil
+    else
+      destination_directory = destination_node.parent
+      destination_file_name = destination_node.name
     end
-    
-    # or if destination node does not exist, but 
+
+    raise InvalidFileError.new(source_node_path) unless access.read?(source_node)
+    raise InvalidFileError.new(destination_directory_name) unless destination_directory && access.write?(destination_directory)
+
+    destination_node.destroy if destination_node
+
+    result_node = source_node.copy(destination_directory,destination_file_name)
 
     output!(NodeJSON.new([result_node]).list_hash)
     return source_node
--- app/models/command/edit.rb
+++ app/models/command/edit.rb
@@ -4,6 +4,11 @@ class Command::Edit < ::Command
 
     node = Node.fetch(node_path)
 
+    if !node
+       directory, file_name = resolve_directory_and_file(argv[1])
+       directory_node = Node.fetch(directory)
+    end
+
     raise InvalidFileError.new(argv[0]) unless access.write?(node) && !node.directory?
     raise InvalidFileError.new(argv[0]) unless node.text?
 
